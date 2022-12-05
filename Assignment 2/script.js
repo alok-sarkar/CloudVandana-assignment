3d8ae8e41ded2321c987a5b25142b30a07f6e0cc
Alok Sarkar <aloksarkar0002@gmail.com>
2022-12-05 12:49:24 +0530

    Assignment 2

---
+++ Assignment 2/script.js
@@ -0,0 +1,17 @@
+let submitButton = document.querySelector('button');
+let output = document.querySelector('#output');
+let sentence = document.querySelector("#sentence");
+let letter = document.querySelector("#letter");
+
+output.style.display = "none";
+
+submitButton.addEventListener('click', () => {
+    var exp = new RegExp(letter.value + "(\.*)", 'i');
+    var data = sentence.value.match(exp);
+    console.log(data);
+    if (data == null)
+        output.children[0].innerHTML = "The letter does not exist in the sentence";
+    else
+        output.children[0].innerHTML = data[1];
+    output.style.display = "block";
+});
